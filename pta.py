f09ea854a9575212ece6f3deba0758dcd0e86e1d
princetongirlye <princeton910207@gmail.com>
2020-05-19 17:43:02 +0800

    pta

--- pta.py
+++ pta.py
@@ -15,7 +15,7 @@ def test_1001():
 # 写出这个数
 def test_1002():
     n = input()
-    py = ['ling','yi', 'er', 'san', 'si', 'wu', 'liu', 'qi', 'ba', 'jiu', 'shi']
+    py = ["ling", "yi", "er", "san", "si", "wu", "liu", "qi", "ba", "jiu", "shi"]
     total = 0
     s = []
     for i in n:
@@ -24,6 +24,7 @@ def test_1002():
         s.append(py[int(i)])
     print(" ".join(s))
 
+
 # 我要通过 --》 审题有误，需要重新考虑
 def test_1003():
     n = int(input())
@@ -31,13 +32,12 @@ def test_1003():
     for i in range(n):
         s = input()
         for j in s:
-            if j not in ['P','A','T']:
+            if j not in ["P", "A", "T"]:
                 flag = False
                 break
 
 
 # 1004 成绩排名
-
 def test_1004():
     n = int(input())
     max, min = 0, 100
@@ -55,6 +55,7 @@ def test_1004():
     print(max_s)
     print(min_s)
 
+
 # 继续 3n+1的猜想
 def test_1005():
     n = int(input())
@@ -71,3 +72,155 @@ def test_1005():
                 x.add(int(i))
     print(" ".join(map(str, sorted(set(nums) - x, reverse=True))))
 
+
+# 换个格式输出整数
+def test_1006():
+    n = input()
+    len_n = len(n)
+    new_s = ""
+
+    if len_n == 2:
+        new_s = "S" * int(n[0])
+    elif len_n == 3:
+        new_s = "B" * int(n[0])
+        new_s += "S" * int(n[1])
+    for i in range(int(n[-1])):
+        new_s += str(i + 1)
+    print(new_s)
+
+
+# 素数对猜想 1007
+def test_1007():
+    pass
+
+
+# 1009 说反话
+def test_1009():
+    s = input().split()
+    print(" ".join(s[::-1]))
+
+
+# 1010 一元多项式求导
+
+# 1011 A+B>C
+def test_1011():
+    n = int(input())
+    for i in range(1, n + 1):
+        a, b, c = input().split()
+        if int(a) + int(b) > int(c):
+            print("Case #" + str(i) + ": true")
+        else:
+            print("Case #" + str(i) + ": false")
+
+
+# 数字分类 1012
+
+def test_1012():
+    nums = list(map(int, input().split()))
+    a1 = []
+    a2 = []
+    a3 = []
+    a4 = []
+    a5 = []
+    all = []
+    for n in nums[1:]:
+        if n % 5 == 0 and n % 2 == 0:
+            a1.append(n)
+        elif n % 5 == 1:
+            a2.append(n)
+        elif n % 5 == 2:
+            a3.append(n)
+        elif n % 5 == 3:
+            a4.append(n)
+        elif n % 5 == 4:
+            a5.append(n)
+    if a1 == []:
+        all.append("N")
+    else:
+        all.append(str(sum(a1)))
+    if a2 == []:
+        all.append("N")
+    else:
+        j = 0
+        for i in range(len(a2)):
+            j += a2[i] * (-1) ** i
+        all.append(str(j))
+    if a3 == []:
+        all.append("N")
+    else:
+        all.append(str(len(a3)))
+    if a4 == []:
+        all.append("N")
+    else:
+        all.append(str(round(sum(a4) / len(a4), 1)))
+    if a5 == []:
+        all.append("N")
+    else:
+        all.append(str(max(a5)))
+    print(" ".join(all))
+
+
+# 1013 数素数 -- 测试点第四个运行超时
+def test_1013():
+    import math
+    m, n = input().split()
+    x = []
+    flag = True
+    k = 0
+    i = 2
+    while flag:
+        for j in range(2, int(math.sqrt(i)) + 1):
+            if i % j == 0:
+                break
+        else:
+            x.append(i)
+        if len(x) == int(n):
+            flag = False
+            break
+        i += 1
+    if int(m) == int(n):
+        print(x[int(m) - 1])
+    else:
+        j = 0
+        print(x)
+        for i in x[int(m) - 1:]:
+            j += 1
+            if j % 10 == 0:
+                print(i)
+            elif j != int(n) - int(m) + 1:
+                print(i, end=" ")
+            else:
+                print(i)
+
+
+# 1014
+def test_1014():
+    s1 = input()
+    s2 = input()
+    s3 = input()
+    s4 = input()
+    DAY = {"A": "MON", "B": "TUE", "C": "WED", "D": "THU", "E": "FRI", "F": "SAT", "G": "SUN"}
+    HH = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I","J", "K", "L","M",
+          "N"]
+    s = []
+    for count in range(min(len(s1), len(s2))):
+        if s1[count] == s2[count]:
+            if s !=[] and s1[count] in HH:
+                s.append(str(HH.index(s1[count])).rjust(2, "0"))
+                break
+            elif s1[count] in DAY.keys():
+                s.append(DAY.get(s1[count]))
+                continue
+
+    for count in range(min(len(s3), len(s4))):
+        if s3[count] == s4[count] and s3[count].isalpha():
+            s.append(str(count).rjust(2, "0"))
+            break
+    print("%s %s:%s" % (s[0], s[1], s[2]))
+
+
+
+# 德才论
+
+def test_1015():
+    pass
